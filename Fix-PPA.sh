498df05d40e8c758cecf556d428115f34f0ccd2a
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2019-06-24 12:25:05 -0400

    Update Fix-PPA.sh

--- Fix-PPA.sh
+++ Fix-PPA.sh
@@ -9,4 +9,5 @@ echo Done.
 sleep 3
 clear
 echo Do this command after the script ends if this does not work: sudo -E add-apt-repository ppa:linrunner/tlp
+echo Goto this site to fix the problem if this did not work: http://opensourceforgeeks.blogspot.com/2013/04/w-gpg-error-httpppalaunchpadnet-precise.html
 sleep 15
