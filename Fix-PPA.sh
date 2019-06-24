895b40b6d88dd6e5025be44653e7ccc410c6b704
Cat-Gaming <47046268+Cat-Gaming@users.noreply.github.com>
2019-06-24 11:11:51 -0400

    Update Fix-PPA.sh

--- Fix-PPA.sh
+++ Fix-PPA.sh
@@ -2,6 +2,11 @@
 echo Reinstalling CA certifcates!
 sudo apt-get install --reinstall ca-certificates
 echo Done.
+echo Updating Linux.
+sudo apt upgrade
+sudo apt update
+echo Done.
+sleep 3
 clear
 echo Do this command after the script ends if this does not work: sudo -E add-apt-repository ppa:linrunner/tlp
 sleep 15
